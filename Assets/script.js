182570a9aad53e0cbf6d73bcd853291f624878ea
[Josie Savill] <[fiordasavilljosie@gmail.com]>
2023-02-04 17:27:43 -0700

    changes to JS, README, HTML

--- Assets/script.js
+++ Assets/script.js
@@ -2,8 +2,55 @@ var pastButtonEl = $('.row time-block past');
 var presentButtonEl = $('.row time-block present');
 var futureButtonEl = $('.row time-block future');
 
-var currentHour = dayjs().hour() // gets current hour
+// adding current date, time to header
+
+// var dateime = new Date();
+// document.getElementById("currenttime").textContent = datetime
+    
+n = new Date();
+y = n.getFullYear();
+m = n.getMonth() + 1;
+d = n.getDate();
+document.getElementById("currentDay").innerHTML = m + "/" + d + "/" + y;
+
+
+var currentHour = dayjs().format("hA") // gets current hour
+
+console.log(currentHour)
+
+
+let allTimeBlocks = document.querySelectorAll(".time-block");
+let foundPresent = false;
+for (let index = 0; index < allTimeBlocks.length; index++) {
+    //currentHOur 4PM
+    let thisTimeBlock =  allTimeBlocks[index].id.split("-")[1];
+    let myText = localStorage.getItem(thisTimeBlock)
+    document.querySelector("#hour-" + thisTimeBlock + " .description").value = myText;
+    console.log(thisTimeBlock, currentHour)
+    if(thisTimeBlock == currentHour){
+        allTimeBlocks[index].classList.add("present") 
+        foundPresent = true;
+    } else if(foundPresent){
+        allTimeBlocks[index].classList.add("future") 
+
+    }
+}
+
+
+
+var allSaveBtn = document.querySelectorAll(".saveBtn");
+
+for (let index = 0; index < allSaveBtn.length; index++) {
+    const element = allSaveBtn[index];
+    element.addEventListener("click", function(e){
+            let buttonKey = e.target.parentNode.parentNode.id.split("-")[1];
+            let myText = document.querySelector("#hour-" + buttonKey + " .description").value // "hour-4PM"
+            localStorage.setItem(buttonKey, myText);
+    })
+}
 // newDate = dayjs().hour(12) // returns new dayjs object
+
+/*
 console.log(currentHour);
 function buildScheduler() {
     for (let index = 9; index <= 18; index++) {
@@ -24,7 +71,7 @@ function buildScheduler() {
     
 }
 buildScheduler()
-
+*/
 
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
--- README.md
+++ README.md
@@ -5,10 +5,10 @@ User Story
 AS AN employee with a busy schedule
 I WANT to add important events to a daily planner
 SO THAT I can manage my time effectively
-
+<!-- add gif of planner -->
 
 Acceptance Criteria
-
+<!-- completed -->
 GIVEN I am using a daily planner to create a schedule
 WHEN I open the planner
 THEN the current day is displayed at the top of the calendar
@@ -16,6 +16,9 @@ WHEN I scroll down
 THEN I am presented with timeblocks for standard business hours
 WHEN I view the timeblocks for that day
 THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+
+<!-- TODO -->
+
 WHEN I click into a timeblock
 THEN I can enter an event
 WHEN I click the save button for that timeblock
